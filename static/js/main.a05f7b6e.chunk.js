5979122a619f0051fb02c8f41010a2ce684ee480
Franco Portin <fportin350@gmail.com>
2021-07-27 17:43:30 -0700

    Updates

---
+++ static/js/main.a05f7b6e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfportin.github.io-react"]=this["webpackJsonpfportin.github.io-react"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var i=n(1),r=n.n(i),c=n(3),o=n.n(c),s=(n(8),n(0));var u=function(){return Object(s.jsx)("h1",{children:"Edited"})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(u,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.a05f7b6e.chunk.js.map
